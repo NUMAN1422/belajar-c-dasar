b3d15b4f9100e35518b4af4c4b09f4907050f05f
Muhamad Nur Rohman <mnurrohman065@gmail.com>
2026-05-08 13:24:03 +0700

    Add files via upload
    tugas uts algoritma dan pemrograman 1

---
+++ segitiga.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double alas ,tinggi ,luas ;
+
+cout << "masukkan alas :";
+cin >> alas;
+
+cout << "masukkan tinggi : ";
+cin >> tinggi;
+
+luas = 0.5 * alas * tinggi ;
+cout << "luas segitiga adalah : " << luas << endl;
+
+return 0;
+
+}
